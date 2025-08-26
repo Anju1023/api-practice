5b6ca205c72be369f7f41d7c2e24b8014f3a7d18
Anju <niafuguapricottaro@gmail.com>
2025-08-26 16:46:03 +0900

    next.config.tsに設定オプションを追加し、Profileページコンポーネントを新規作成しました。

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	output: 'export',
+	trailingSlash: true,
+	basePath: '',
+	assetPrefix: '',
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
